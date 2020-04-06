4f813798927f470ec442a390133218a84f69db0b
Colin Park <colin@Colins-MacBook-Pro-15.local>
2020-04-06 23:58:14 +0900

    changed server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 
-const PORT = 9000;
+//const PORT = 9000;
+const PORT = process.env.PORT || 9000;
 
 app.use(express.static('dist'));
 app.use(express.static('./'));
@@ -10,5 +11,8 @@ app.get('/', function(req, res) {
   res.sendfile('index.html');
 });
 
-app.listen(PORT);
-console.log(`[SERVER RUNNING] 127.0.0.1:${PORT}`);
+//app.listen(PORT);
+//console.log(`[SERVER RUNNING] 127.0.0.1:${PORT}`);
+app.listen(PORT, function(){
+  console.log("Server listening on port %d in %s mode", PORT, app.settings.env);
+});
