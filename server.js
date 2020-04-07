36b8038e5adb48f356763f84884a8d967c575641
Colin Park <colin@Colins-MacBook-Pro-15.local>
2020-04-08 03:35:36 +0900

    server

--- server.js
+++ server.js
@@ -15,26 +15,54 @@ var headers = {
   'Api-Token': '7992c4dad48ff10c7e5369d3cc5a271901bf6b10'
 };
 
+// GET
 app.get('/', function(req, res) {
   res.sendfile('index.html');
 });
 
+// POST - for Webhook
 app.post("/webhook", (req, res) => {
   console.log("Received POST message from the server");
   res.sendStatus(200);  // responds with 200 - successfully received
   console.log(req.body);
 
-  const ChannelURL = req.body.channel.channel_url;
+  var ChannelURL = req.body.channel.channel_url;
+
+  // const ChannelURL = req.body.channel.channel_url;
+  // console.log('Channel url => ' + ChannelURL);
+  // const AdminMsg = {
+  //   "message_type":"ADMM",
+  //   "message": "Welcome to Group Channel!"
+  // }
+  // console.log('Admin msg =>' + JSON.stringify(AdminMsg));
+
+  // var options = {
+  //   host: "https://api-F58FD34C-1E49-41AE-ACB4-F82B889ABAE1.sendbird.com/v3/group_channels/" + ChannelURL + "/messages",
+  //   path: '/',
+  //   method: 'POST',
+  //   json: true,
+  //   header: headers,
+  //   body: AdminMsg
+  // };
+  // console.log('Data to send =>' + JSON.stringify(options));
+
+  // // Request admin message
+  // var http = require('http'); 
+  // var req = http.request(options, function(res) {
+  // });
+  
+  // req.write(userString);
+  // req.end();
+});
   console.log('Channel url => ' + ChannelURL);
-  const AdminMsg = {
+  var AdminMsg = {
     "message_type":"ADMM",
     "message": "Welcome to Group Channel!"
   }
   console.log('Admin msg =>' + JSON.stringify(AdminMsg));
 
   var options = {
-    host: "https://api-F58FD34C-1E49-41AE-ACB4-F82B889ABAE1.sendbird.com/v3/group_channels/" +    
-      ChannelURL + "/messages",
+    host: "https://api-F58FD34C-1E49-41AE-ACB4-F82B889ABAE1.sendbird.com/v3/group_channels/" + ChannelURL + "/messages",
     path: '/',
     method: 'POST',
     json: true,
@@ -48,12 +76,7 @@ app.post("/webhook", (req, res) => {
   var req = http.request(options, function(res) {
   });
   
-  req.write(userString);
-  req.end();
-});
   
-
-//res.json(users);    
 
 //app.listen(PORT);
 //console.log(`[SERVER RUNNING] 127.0.0.1:${PORT}`);
