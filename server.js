b969807333c7233c5b3b3b582a282dd04e4e2719
Colin Park <colin@Colins-MacBook-Pro-15.local>
2020-04-08 05:12:48 +0900

    server

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ app.use(express.static('dist'));
 app.use(express.static('./'));
 app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: true }));
-  
-var headers = {
-  'Content-Type': 'application/json, charset=utf8',
-  'Api-Token': '541a1e5f356158b8d337d00cdb08df754ba4efc1' // Secondary API token
-};
 
+// Body of admin message 
+var post_data = querystring.stringify({
+  'message_type': 'ADMM',
+  'message': 'Welcome to Group Channel!'
+});
+  
 // GET
 app.get('/', function(req, res) {
   res.sendfile('index.html');
@@ -23,33 +24,29 @@ app.get('/', function(req, res) {
 // POST - for Webhook
 app.post("/webhook", (req, res) => {
   console.log("Received POST message from the server");
-  res.sendStatus(200);  // responds with 200 - successfully received
+  res.sendStatus(200);  // responds to the server with 200 - successfully received
   console.log(req.body);
 
   const ChannelURL = req.body.channel.channel_url;
-  console.log('Channel url => ' + ChannelURL);
-  const AdminMsg = {
-    "message_type":"ADMM",
-    "message": "Welcome to Group Channel!"
-  }
-  console.log('Admin msg => ' + JSON.stringify(AdminMsg));
 
-  var options = {
+  var post_options = {
     host: 'api-F58FD34C-1E49-41AE-ACB4-F82B889ABAE1.sendbird.com',
     path: '/v3/group_channels/' + ChannelURL + '/messages',
     method: 'POST',
-    //json: true,
-    header: headers,
-    body: AdminMsg
+    header: {
+      'Content-Type': 'application/json, charset=utf8',
+      'Api-Token': '541a1e5f356158b8d337d00cdb08df754ba4efc1' // Secondary API token
+    }
   };
-  console.log('Data to send =>' + JSON.stringify(options));
-
-  // Request admin message
-  var https = require('https'); 
-  var req = https.request(options, function(res) {
-    console.log('Status => ' + res.statusCode);
+  console.log('Data to send =>' + JSON.stringify(post_options));
+  
+  var req = http.request(post_options, function(res) {
+    res.on('data', function (chunk) {
+        console.log('Response => ' + chunk);
+    });
   });
 
+  req.write(post_data);
   req.end();  // No more request
 });
 
