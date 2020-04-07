b1725f17a056564d54dfd37f318928243b11b4a2
Colin Park <colin@Colins-MacBook-Pro-15.local>
2020-04-08 07:42:06 +0900

    server

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ var timestamp = + new Date();
 var post_data = JSON.stringify({
   "message_type": "ADMM",
   "message": 
-  "******************************<br />" + 
-  "*   Welcome to Group Chat!   *<br />" +
-  "******************************<br />",
+  "******************************\\\n" + 
+  "*   Welcome to Group Chat!   *\\\n" +
+  "******************************\\\n",
   "created_at": timestamp 
 });
 var DateCreated = new Date(timestamp * 1000);
